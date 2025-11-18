b6900e4ba19e10e618e871d16bbe90cb5faabdd5
Yuwei Xiao <ywxiaozero@gmail.com>
2025-11-18 14:52:50 +0800

    Install mcp using zed extension

--- src/mcp_server_relytone.rs
+++ src/mcp_server_relytone.rs
@@ -1,16 +1,20 @@
 use schemars::JsonSchema;
 use serde::Deserialize;
+use std::env;
 use zed::settings::ContextServerSettings;
 use zed_extension_api::{
     self as zed, serde_json, Command, ContextServerConfiguration, ContextServerId, Project, Result,
 };
 
+const PACKAGE_NAME: &str = "@relyt/mcp-server-relytone";
+const SERVER_PATH: &str = "node_modules/@relyt/mcp-server-relytone/dist/index.js";
+
+struct RelytONEModelContextExtension;
+
 #[derive(Debug, Deserialize, JsonSchema)]
 struct RelytONEContextServerSettings {
     relytone_bearer_token: String,
 }
-
-struct RelytONEModelContextExtension;
 
 impl zed::Extension for RelytONEModelContextExtension {
     fn new() -> Self {
@@ -22,6 +26,12 @@ impl zed::Extension for RelytONEModelContextExtension {
         _context_server_id: &ContextServerId,
         project: &Project,
     ) -> Result<Command> {
+        let latest_version = zed::npm_package_latest_version(PACKAGE_NAME)?;
+        let installed_version = zed::npm_package_installed_version(PACKAGE_NAME)?;
+        if installed_version.as_deref() != Some(latest_version.as_ref()) {
+            zed::npm_install_package(PACKAGE_NAME, &latest_version)?;
+        }
+
         let settings = ContextServerSettings::for_project("mcp-server-relytone", project)?;
         let Some(settings) = settings.settings else {
             return Err("missing `relytone_bearer_token` setting".into());
@@ -29,15 +39,16 @@ impl zed::Extension for RelytONEModelContextExtension {
         let settings: RelytONEContextServerSettings =
             serde_json::from_value(settings).map_err(|e| e.to_string())?;
 
+        let node_path = zed::node_binary_path()?;
+        let server_path = env::current_dir().unwrap()
+            .join(SERVER_PATH)
+            .to_string_lossy()
+            .to_string();
+
         Ok(Command {
-            command: "npx".into(),
-            args: vec!["--yes".into(), "@relyt/mcp-server-relytone".into()],
-            env: vec![
-                (
-                    "RELYTONE_BEARER_TOKEN".into(),
-                    settings.relytone_bearer_token,
-                ),
-            ],
+            command: node_path,
+            args: vec![server_path],
+            env: vec![("RELYTONE_BEARER_TOKEN".into(), settings.relytone_bearer_token)],
         })
     }
 
